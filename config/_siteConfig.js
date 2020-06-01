8c53d5ab12334457d36553b3ba5a0e8fd55e1731
Alex Filipenko <alex@MacBook-Pro-Alex.local>
2020-06-01 23:21:22 +0300

    integrated mailchimp

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -74,7 +74,7 @@ export default {
     mailchimp: {
       on: true,
       formAction:
-        'https://danielkelly.us3.list-manage.com/subscribe/post?u=d3c2e762b66a2353d0bc29bf3&amp;id=2abeaa06e0'
+        'https://velhostpro.us10.list-manage.com/subscribe/post?u=9546ca6d2653942757e7a9309&amp;id=6c9ee85e14'
     },
     custom: {
       on: false,
