2ca84bf6d8474a4e40c5310fc28332bcb764c256
Alex Filipenko <alex@MacBook-Pro-Alex.local>
2020-06-01 22:22:55 +0300

    Added google analytics ID

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -15,7 +15,7 @@ export default {
   logo: 'logo-component', // 'logo-component', // or '/logo.svg' for regular image
   googleAnalytics: {
     on: true,
-    id: process.env.GOOGLE_ANALYTICS_ID
+    id: UA-168155816-1
   },
   mainMenu: [
     {
